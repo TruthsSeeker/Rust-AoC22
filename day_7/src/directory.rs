68c3345c74b88c95112b137811a8ae6b10bcfa1d
Loic Heinrich <heinrichloic@gmail.com>
2023-08-15 21:59:15 +0200

    day 7 ls behaving properly

--- day_7/src/directory.rs
+++ day_7/src/directory.rs
@@ -1,16 +1,17 @@
-use std::{collections::VecDeque, cell::RefCell, rc::{Weak, Rc}, borrow::BorrowMut};
+use std::{collections::{VecDeque, HashMap}, cell::{Cell, RefCell}, rc::Weak};
 
 pub struct Directory {
-    pub value: i32,
+    pub value: usize,
     pub name: String,
     pub parent: Weak<RefCell<Directory>>,
     pub children: Vec<Directory>,
+    pub files: HashMap<String, usize>
 }
 
 
 
 impl Directory {
-    fn dfs(self: &Self, value: i32) -> Option<&Directory> {
+    fn dfs(self: &Self, value: usize) -> Option<&Directory> {
         if self.value == value {
             return Some(self)
         }
@@ -21,7 +22,7 @@ impl Directory {
         None
     }
 
-    fn bfs(self: &Self, value: i32) -> Option<&Directory> {
+    fn bfs(self: &Self, value: usize) -> Option<&Directory> {
         let mut queue = VecDeque::new();
         queue.push_back(self);
 
@@ -37,7 +38,7 @@ impl Directory {
         None
     }
 
-    fn depth_first_traversal(self: &Self, lambda: fn(&i32) ) {
+    fn depth_first_traversal(self: &Self, lambda: fn(&usize) ) {
         lambda(&self.value);
 
         for child in &self.children {
@@ -45,7 +46,7 @@ impl Directory {
         }
     }
 
-    fn breadth_first_traversal(self: &Self, lambda: fn(&i32)) {
+    fn breadth_first_traversal(self: &Self, lambda: fn(&usize)) {
         let mut queue = VecDeque::new();
         queue.push_back(self);
 
@@ -58,11 +59,18 @@ impl Directory {
         }
     }
 
-    fn sum_children(self: &Self) -> i32 {
+    fn sum_children(self: &Self) -> usize {
         let mut sum = 0;
         for child in &self.children {
             sum += child.value;
         }
         sum
+    }
+
+    pub fn propagate_value(self: &mut Self, value: usize) {
+        self.value += value;
+        if let Some(parent) = self.parent.upgrade() {
+            parent.borrow_mut().propagate_value(value);
+        }
     }
 }
--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, collections::{HashMap, VecDeque}, rc::{Rc, Weak}, cell::{RefCell, Cell}};
 mod directory;
 use directory::Directory;
 
@@ -11,6 +11,7 @@ enum LineType {
 }
 
 fn main() {
+    let mut cursor: Option<&mut Directory> = None;
 
 }
 
@@ -31,6 +32,44 @@ fn parse_line_type(line: &str) -> LineType {
     }
 }
 
+fn parse_file(line: &str) -> (usize, &str) {
+    let mut iter = line.split_whitespace();
+    let value = iter.next().unwrap().parse::<usize>().unwrap();
+    let name = iter.next().unwrap();
+    (value, name)
+}
+
+fn parse_dir(line: &str) -> &str {
+    let mut iter = line.split_whitespace();
+    iter.next(); // skip the "dir" part
+    iter.next().unwrap()
+}
+
+fn process_ls<'a>(parent: &Rc<RefCell<Directory>>, name: &str, mut contents: VecDeque<&'a str>) -> VecDeque<&'a str> {
+    let mut files = HashMap::new();
+    while let Some(line) = contents.pop_front() {
+        match parse_line_type(line) {
+            LineType::Command => break,
+            LineType::File => {
+                let (size, file) = parse_file(line);
+                files.insert(file.to_string(), size);
+            },
+            LineType::Directory => continue,
+        };
+        
+    }
+    let value = files.values().sum();
+    let directory = Directory {
+        value: value ,
+        name: name.to_string(),
+        parent: Rc::downgrade(&parent),
+        children: vec![],
+        files: files,
+    };
+    parent.borrow_mut().children.push(directory);
+    parent.borrow_mut().propagate_value(value);
+    contents
+}
 
 
 
@@ -43,5 +82,34 @@ mod tests {
         assert_eq!(parse_line_type("$ ls"), LineType::Command);
         assert_eq!(parse_line_type("dir dfgjdlk"), LineType::Directory);
         assert_eq!(parse_line_type("123456 dfgag"), LineType::File);
+    }
+
+    #[test]
+    fn test_parse_file() {
+        assert_eq!(parse_file("123456 dfgag"), (123456, "dfgag"));
+    }
+
+    #[test]
+    fn test_parse_dir() {
+        assert_eq!(parse_dir("dir dfgjdlk"), "dfgjdlk");
+    }
+
+    #[test]
+    fn test_process_ls() {
+        let test_data = load_file("data/test.txt").unwrap();
+        let mut contents = test_data.lines().collect::<VecDeque<&str>>();
+        contents.pop_front(); // remove $ cd /
+        contents.pop_front(); // remove $ ls
+        let parent = Directory {
+            value: 0,
+            name: "root".to_string(),
+            parent: Weak::new(),
+            children: vec![],
+            files: HashMap::new(),
+        };
+        let parent_rc = Rc::new(RefCell::new(parent));
+        let contents = process_ls(&parent_rc, "root", contents);
+        assert_eq!(parent_rc.borrow().children.len(), 1);
+        assert_eq!(parent_rc.borrow().value, 23352670);
     }
 }
